80853785bfa529b64390ff8e9b63452ec2fcbf03
Pablo Borlaf Mena <borlafdev@gmail.com>
2014-03-04 21:27:02 +0100

    Added sample fork process and receives the status exit code

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,40 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
+int child_status;
+char * arg_list[] = {"ls",
+                     "/",
+                     NULL };
+
+int spawn(char * program, char ** arg){
+    pid_t child_pid=0;
+    child_pid=fork();
+     if (child_pid!=0){
+          return child_pid;
+     }
+     else{
+        execvp(program, arg);
+     }
+
+}
+/***************/
+
+struct web_info{
+    QString url;
+    int time;
+};
+
+void* factorial(){
+
+}
+
+/***/
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -11,4 +45,19 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+
+    spawn("ls", arg_list);
+
+    wait(&child_status);
+    if(WIFEXITED(child_status)){
+      ui->prResult->setText(QString::number(WEXITSTATUS(child_status)));
+
+    }else{
+    ui->prResult->setText("Execution failed");
+    }
+
 }
